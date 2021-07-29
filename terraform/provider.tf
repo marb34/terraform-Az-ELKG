d1d072de278fb699ebe260f1c40e24fb82e7cfea
mramirez <8210+mramirez@users.noreply.github.sie.sony.com>
2021-07-28 22:15:04 -0400

    Adding new tenant and ignores

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,9 +2,13 @@ provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
   version = "~>2.4.0"
 
-  subscription_id = "07434f58-9ca2-4a76-b72f-e615836f2bb4"
-  client_id       = "6f9d1404-8051-464b-a6bf-366de7caeb4f"
-  tenant_id       = "a758a091-0414-46bd-9e89-e5139d903d34"
-
   features {}
+  #subscription_id = "c77c8b8e-3bde-4978-bf08-5540d3ec037c"
+  subscription_id = "455bc8ec-de3b-408a-9abe-5f296379d983"
+  #client_id = "b88b40e3-b484-43fd-b6b6-90e45c73e1a5"
+  client_id = "617a4f24-58c8-4178-a4d3-34bd65c6bbbe"
+  #client_secret = "5_x9~~u1s-_u_G-3.OR6dPjS6e26a3jvj0"
+  client_secret = "-2yQdi4RjHM.ULt1trt6nn5_hxnMJX~A8-"
+  #tenant_id = "2af0146a-276c-44c6-a265-c6be5baedce1"
+  tenant_id = "a758a091-0414-46bd-9e89-e5139d903d34"
 }
